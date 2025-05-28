8447cb055e1c8ffdc975121d8a72a357bef8108a
SUNHaoran <13592589856@163.com>
2025-05-28 15:14:17 +0200

    Add files via upload

---
+++ src/models/DECOMP.py
@@ -0,0 +1,175 @@
+import numpy as np
+from scipy.optimize import root
+
+def find_root(x, n):
+    if x < 0:
+        g = 1j * pow(-x, n)
+    else:
+        g = pow(x, n)
+    return g
+
+
+def compressor(x, fs, L, R, T_v_att, T_v_rel, T_g_att, T_g_rel, p=2, normalize_after=False):
+    # L The threshold in dB
+    # R The compression ratio
+    # p The detector type (RMS=2 or peak=1)
+    # T_v_att The attack time of the envelope filter in ms
+    # T_v_rel The release time of the envelope filter in ms
+    # T_g_att The attack time of the gain filter in ms
+    # T_g_rel The release time of the gain filter in ms
+
+    if max(np.abs(x)) > 1:
+        return print('Warning: x should be normalized!')
+
+    BG = 1 - np.exp(-2.2 / (np.array([T_v_att, T_v_rel, T_g_att, T_g_rel]) / 1000 * fs))
+
+    Batt = BG[0]
+    # Batt = 1
+    Brel = BG[1]
+    # Brel = 1
+    Gatt = BG[2]
+    # Gatt = 1
+    Grel = BG[3]
+    # Grel = 1
+
+    S = 1 - 1 / R  # Slope
+    l = 10 ** (L / 20)  # l in linear scale
+    K = l ** S
+    N = len(x)
+
+    B_init = 1  # Initial value
+    G_init = 1  # Initial value
+    x2 = np.zeros(N)  # x_tilde
+    v = np.zeros(N)
+    f = np.ones(N)
+    g = np.zeros(N)
+
+    for n in range(N):
+        if n == 0:  # Initialization
+            x2[n] = B_init * np.abs(x[n]) ** p
+        else:
+            if np.abs(x[n]) > v[n - 1]:  # Attack if x is greater than envelope
+                x2[n] = Batt * np.abs(x[n]) ** p + (1 - Batt) * x2[n - 1]
+            else:  # Release if x is lower than envelope
+                x2[n] = Brel * np.abs(x[n]) ** p + (1 - Brel) * x2[n - 1]
+
+        v[n] = find_root(x2[n], 1 / p)
+        # v[n] = x2[n] ** (1 / p)
+
+        # compress or not
+        if v[n] > l:
+            f[n] = K * find_root(v[n], -S)
+            # f[n] = K * (v[n] ** (-S))
+        else:
+            f[n] = 1
+
+        if n == 0:
+            g[n] = G_init * f[n]
+        else:
+            if f[n] < g[n - 1]:  # Attack
+                g[n] = Gatt * f[n] + (1 - Gatt) * g[n - 1]
+            else:  # Release
+                g[n] = Grel * f[n] + (1 - Grel) * g[n - 1]
+
+    y = g * x
+
+    I = np.isnan(y)
+    I2 = np.where(g < 0)
+    if np.any(I) or len(I2[0]) > 0:
+        print('Warning: Invalid parameters')
+        y[I] = 0
+        y = np.abs(y) * np.sign(x)
+
+    if normalize_after:
+        y = y - np.mean(y)
+        y = y / np.max(np.abs(y))
+
+    return y
+
+
+def Xip(v, eps, B, G, K, S, p, g, y, x_tilde):
+    return find_root(G * K * find_root(v, -S) + (1 - G) * g, p) * (find_root(v, p) - (1 - B) * x_tilde) - B * find_root(np.abs(y), p) + eps
+
+### original charzero function
+# def CHARFZERO(vn, eps, B, G, K, S, p, g, y, x_tilde):  # find the root of Xip
+#     vi = vn
+#     xipvi = Xip(vi, eps, B, G, K, S, p, g, y, x_tilde)
+#     continuer = True
+#     while continuer:
+#         Di = np.abs(xipvi)
+#         xipvi_Di = Xip(vi + Di, eps, B, G, K, S, p, g, y, x_tilde)
+#         vi = vi - Di * xipvi / (xipvi_Di - xipvi + eps)
+#         xipvi = Xip(vi, eps, B, G, K, S, p, g, y, x_tilde)
+#         if np.abs(xipvi) > Di:
+#             v0 = vn
+#             return v0
+#         vn = vi
+#         continuer = np.abs(xipvi) > eps
+#     v0 = vi
+#     return v0
+
+# ### Scipy function
+def CHARFZERO(vn, eps, B, G, K, S, p, g, y, x_tilde):
+    root_result = root(Xip, vn, args=(eps, B, G, K, S, p, g, y, x_tilde))
+    return root_result.x
+
+
+def decompressor(y, Fs, L, R, T_v_att, T_v_rel, T_g_att, T_g_rel, p, eps=np.finfo(float).eps, normalize_after=False):
+
+    if max(np.abs(y)) > 1:
+        y = y - np.mean(y)
+        y = y / np.max(np.abs(y))
+
+    # Convert time to filter coefficients
+    BG = 1 - np.exp(-2.2 / (np.array([T_v_att, T_v_rel, T_g_att, T_g_rel]) / 1000 * Fs))
+
+    S = 1 - 1 / R
+    l = 10 ** (L / 20)
+    K = l ** S  # root(l, S)
+    N = len(y)
+    x = np.zeros(N)
+    g = np.zeros(N)
+    v = np.zeros(N)
+    x2 = np.zeros(N)
+
+    # x_tilde = 0
+    x[0] = y[0]
+    g[0] = 1
+
+    for n in range(N):
+        if np.abs(y[n]) > find_root(x2[n], 1 / p) * g[n]:
+            B = BG[0]
+        else:  # Release
+            B = BG[1]
+
+        if np.abs(y[n]) > find_root((find_root((K / g[n]), p / S) - (1 - B) * x2[n]) / B, 1 / p) * g[n]:  # Attack
+            G = BG[2]
+        else:
+            G = BG[3]
+        if np.abs(y[n]) > find_root((find_root(l, p) - (1 - B) * x2[n]) / B, 1 / p) * (G + (1 - G) * g[n]):
+            v[n] = find_root(B * find_root(np.abs(y[n]) / (G + (1 - G) * g[n]), p) + (1 - B) * x2[n], 1 / p)
+            if n > 0:
+                v0 = CHARFZERO(v[n], eps, B, G, K, S, p, g[n - 1], y[n], x2[n])
+            else:
+                v0 = CHARFZERO(v[n], eps, B, G, K, S, p, 1, y[n], x2[n])
+            x[n] = abs(find_root((find_root(v0, p) - (1 - B) * x2[n]) / B, 1 / p))
+            x2[n] = v0 ** p
+            g[n] = np.abs(y[n]) / np.abs(x[n])
+
+        else:
+            g[n] = G + (1 - G) * g[n]
+            x[n] = np.abs(y[n]) / g[n]
+            x2[n] = B * find_root(np.abs(x[n]), p) + (1 - B) * x2[n]
+
+        x[n] = np.sign(y[n]) * np.abs(x[n])
+
+        if n < N - 1:
+            g[n + 1] = g[n]
+            v[n + 1] = v[n]
+            x2[n + 1] = x2[n]
+
+    if normalize_after:
+        x = x - np.mean(x)
+        x = x / np.max(np.abs(x))
+
+    return x
---
+++ src/models/tfe_model.py
@@ -0,0 +1,261 @@
+﻿import torch
+import torchaudio
+import numpy as np
+import nnAudio.features
+import librosa
+from torch_stft import STFT
+
+class TimeCQT_Encoder(torch.nn.Module):
+    def __init__(
+            self,
+            samplerate: int = 16000,
+            n_bins: int = 113,
+            conv_class=torch.nn.Conv2d,
+            NL_class=torch.nn.PReLU,
+            out_channels=64,
+            kernel_size=[1, 128],
+            device=None,
+            eps=1e-6,
+            compute_representation=True
+    ):
+        super(TimeCQT_Encoder, self).__init__()
+        self.n_bins = n_bins
+
+        self.eps = eps
+
+        self.out_dim = out_channels
+        self.compute_representation = compute_representation
+        self.name = "ENC_TimeCQT"
+        if compute_representation:
+            self.cqt = nnAudio.features.CQT1992v2(
+                n_bins=n_bins,
+                sr=samplerate
+            )
+            print(f"CQT kernel width : {self.cqt.kernel_width}")
+
+        self.conv1 = torch.nn.Sequential(
+            conv_class(
+                in_channels=1,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+            ),
+            torch.nn.BatchNorm2d(num_features=out_channels),
+            NL_class()
+        )
+        self.conv2 = torch.nn.Sequential(
+            torch.nn.Conv1d(
+                in_channels=out_channels,
+                out_channels=out_channels,
+                kernel_size=31   # 31
+            ),
+            torch.nn.BatchNorm1d(num_features=out_channels),
+            NL_class()
+        )
+
+    def forward(self, x):
+        batch_size = x.size(0)
+
+        if self.compute_representation:
+            x = self.cqt(x).reshape(batch_size, 1, self.n_bins, -1)
+            x = torch.log(x + self.eps)
+
+        x = self.conv1(x)
+        x, _ = torch.max(x, dim=2)
+        # x = torch.mean(x, dim=2)
+        x = self.conv2(x)
+        x = x.mean(2)  # Mean across time frames
+        x = x.reshape(batch_size, self.out_dim)  # Mean across frequency bins
+        return x
+
+
+class FrequencyCQT_Encoder(torch.nn.Module):
+    def __init__(
+            self,
+            samplerate: int = 16000,
+            n_bins: int = 113,
+            conv_class=torch.nn.Conv2d,
+            NL_class=torch.nn.PReLU,
+            out_channels=64,
+            kernel_size=[37, 1],
+            device=None,
+            eps=1e-6,
+            compute_representation=True
+    ):
+        super(FrequencyCQT_Encoder, self).__init__()
+        self.n_bins = n_bins
+
+        self.eps = eps
+
+        self.out_dim = out_channels
+        self.compute_representation = compute_representation
+        self.name = "ENC_FrequencyCQT"
+
+        if compute_representation:
+            self.cqt = nnAudio.features.CQT1992v2(
+                n_bins=n_bins,
+                sr=samplerate
+            )
+            print(f"CQT kernel width : {self.cqt.kernel_width}")
+
+        self.conv1 = torch.nn.Sequential(
+            conv_class(
+                in_channels=1,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+            ),
+            torch.nn.BatchNorm2d(num_features=out_channels),
+            NL_class()
+        )
+        self.conv2 = torch.nn.Sequential(
+            torch.nn.Conv1d(
+                in_channels=out_channels,
+                out_channels=out_channels,
+                kernel_size=n_bins - kernel_size[0] + 1
+            ),
+            torch.nn.BatchNorm1d(num_features=out_channels),
+            NL_class()
+        )
+
+    def forward(self, x):
+        batch_size = x.size(0)
+
+        if self.compute_representation:
+            x = self.cqt(x).reshape(batch_size, 1, self.n_bins, -1)
+            x = torch.log(x + self.eps)
+
+        x = self.conv1(x)
+        x, _ = torch.max(x, dim=3)
+        # x = torch.mean(x, dim=3)
+        # x = x.mean(2) # Mean across time frames
+        x = self.conv2(x)
+        x = x.reshape(batch_size, self.out_dim)  # Mean across frequency bins
+        return x
+
+
+class TFE(torch.nn.Module):
+    def __init__(
+            self,
+            samplerate: int = 16000,
+            f_dim: int = 64,
+            t_dim: int = 431,
+            label_dim: int = 6,
+            conv_class=torch.nn.Conv2d,
+            NL_class=torch.nn.PReLU,
+            device=None,
+            eps=1e-6,
+            compute_representation=True
+    ):
+        super(TFE, self).__init__()
+        self.samplerate = samplerate
+        self.f_dim = f_dim
+        self.t_dim = t_dim
+        self.label_dim = label_dim
+        self.eps = eps
+
+        self.compute_representation = compute_representation
+
+        # if self.compute_representation:
+        #     total_samples = 5 * samplerate
+        #     n_fft = 2 * (f_dim - 1)
+        #     hop_length = (total_samples - n_fft) // (t_dim - 1)
+        #     self.stft = torch.stft(
+        #         n_fft=n_fft,
+        #         hop_length=hop_length,
+        #     )
+
+        self.time = TimeCQT_Encoder(samplerate=self.samplerate, n_bins=self.f_dim, NL_class=NL_class, compute_representation=False)
+        self.frequency = FrequencyCQT_Encoder(samplerate=self.samplerate, n_bins=self.f_dim, NL_class=NL_class, compute_representation=False)
+        self.out_dim = self.time.out_dim + self.frequency.out_dim
+        self.mlp = torch.nn.Linear(self.out_dim, self.label_dim)
+
+    def forward(self,x):
+
+        batch_size = x.size(0)
+
+        if self.compute_representation:
+            total_samples = 5 * self.samplerate
+            n_fft = 2 * (self.f_dim - 1)
+            hop_length = (total_samples - n_fft) // (self.t_dim - 1)
+            # self.stft = torch.stft(
+            #     n_fft=n_fft,
+            #     hop_length=hop_length,
+            # )
+            x = x.squeeze(1)
+            x = torch.stft(x, n_fft=n_fft, hop_length=hop_length, return_complex=True)
+            # x = x.reshape(batch_size, 1, self.n_bins, -1)
+            x = torch.abs(x)
+            x = torch.log(x + self.eps)
+            x = x.unsqueeze(1)
+
+        out = torch.zeros((batch_size, self.out_dim), device=x.device)
+
+        out[:, :self.time.out_dim] = self.time(x)
+        out[:, self.time.out_dim:] = self.frequency(x)
+        out = self.mlp(out)
+
+        return out
+
+class TimeFrequencyCQT_Encoder(torch.nn.Module):
+    def __init__(
+            self,
+            samplerate: int = 16000,
+            n_bins: int = 64,
+            label_dim = 6,
+            conv_class=torch.nn.Conv2d,
+            NL_class=torch.nn.PReLU,
+            device=None,
+            eps=1e-6,
+            compute_representation=True
+    ):
+        super(TimeFrequencyCQT_Encoder, self).__init__()
+        self.n_bins = n_bins
+        self.label_dim = label_dim
+        self.eps = eps
+
+        self.compute_representation = compute_representation
+        self.name = "ENC_TimeFrequencyCQT"
+
+        if self.compute_representation:
+            self.cqt = nnAudio.features.CQT1992v2(
+                n_bins=n_bins,
+                sr=samplerate
+            )
+            print(f"CQT kernel width : {self.cqt.kernel_width}")
+
+        self.time = TimeCQT_Encoder(n_bins=self.n_bins, NL_class=NL_class, compute_representation=False)
+        self.frequency = FrequencyCQT_Encoder(n_bins=self.n_bins, NL_class=NL_class, compute_representation=False)
+        self.out_dim = self.time.out_dim + self.frequency.out_dim
+        self.mlp = torch.nn.Linear(self.out_dim, self.label_dim)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+
+        if self.compute_representation:
+            x = self.cqt(x).reshape(batch_size, 1, self.n_bins, -1)
+            x = torch.log(x + self.eps)
+
+        out = torch.zeros((batch_size, self.out_dim), device=x.device)
+
+        out[:, :self.time.out_dim] = self.time(x)
+        out[:, self.time.out_dim:] = self.frequency(x)
+        out = self.mlp(out)
+
+        return out
+
+if __name__ == '__main__':
+
+    batch_size = 12
+    sr = 16000
+    input_length = sr * 5
+    f_dim = 64
+    t_dim = 431
+
+    input = torch.rand(batch_size, 1, input_length)
+    print(f"Input Shape : {input.shape}\n")
+
+    network = TimeFrequencyCQT_Encoder(samplerate=sr, n_bins=113)
+    # network = TFE(samplerate=sr, f_dim=f_dim, t_dim=t_dim, label_dim=6)
+
+    # model inference
+    output = network(input)
+    print(f"Output Shape : z={output.shape}")
